9eab003671dde8727dd392b60aaba7948f570426
gsgsghshhshz <kshshhahzhzhahjzjzjzj@atomicmail.io>
2026-08-08 19:39:59 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-}
-
-android {
-    namespace = "com.example.luminaplayer"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.luminaplayer"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildFeatures {
-        compose = true
-    }
-}
-
-dependencies {
-    implementation(libs.media3.exoplayer)
-    implementation(libs.media3.ui)
-    implementation(libs.media3.decoder.av1)
-    implementation(libs.media3.datasource)
-
-    implementation(platform(libs.compose.bom))
-    implementation(libs.compose.material3)
-    implementation(libs.lifecycle.viewmodel.compose)
-    implementation(libs.lifecycle.runtime.compose)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
